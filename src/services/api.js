742d5c53e87d60b5f67daeb818017642f885b976
Renan Fernandes <renzinestuods@gmail.com>
2023-04-28 18:59:26 -0300

    :sparkles: feat: Cria a função getProduct

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,21 @@
-const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
+const BASE_URL = 'https://api.mercadolibre.com';
 
-async function fetchAPI(url) {
+export default async function fetchAPI(url) {
   const response = await fetch(url);
   return response.json();
 }
 
 export async function getCategories() {
-  const url = `${BASE_URL}/categories`;
+  const url = `${BASE_URL}/sites/MLB/categories`;
   return fetchAPI(url);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `${BASE_URL}/search?category=$${categoryId}&q=$${query}`;
+  const url = `${BASE_URL}/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  return fetchAPI(url);
+}
+
+export async function getProduct(id) {
+  const url = `${BASE_URL}/items/${id}`;
   return fetchAPI(url);
 }
