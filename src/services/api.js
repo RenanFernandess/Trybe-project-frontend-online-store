c0a67600378adf18f38c868cc453c54a9b2fa68a
Renan Fernandes <renzinestuods@gmail.com>
2023-04-28 18:44:58 -0300

    :recycle: refactor: Refatora as funções getCategories e getProductsFromCategotyAndQuery

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,16 @@
-export async function getCategories() {
-  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
+
+async function fetchAPI(url) {
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  return response.json();
+}
+
+export async function getCategories() {
+  const url = `${BASE_URL}/categories`;
+  return fetchAPI(url);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const url = `${BASE_URL}/search?category=$${categoryId}&q=$${query}`;
+  return fetchAPI(url);
 }
