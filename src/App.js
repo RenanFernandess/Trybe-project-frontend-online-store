e3acf58348478373b10753142bfafba01e2657e5
Renan Fernandes <renzinestuods@gmail.com>
2023-05-01 12:11:47 -0300

    :sparkles: feat: Configura o ProductProvider e renderiza o componente Header

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './pages/Search';
 import ShoppingCart from './pages/ShoppingCart';
 import Product from './pages/Product';
+import { ProductProvider } from './context';
+import Header from './components';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={ Search } />
-          <Route exact path="/ShoppingCart" component={ ShoppingCart } />
-          <Route exact path="/Product/:id" component={ Product } />
-        </Switch>
-      </div>
+      <ProductProvider>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={ Search } />
+            <Route exact path="/ShoppingCart" component={ ShoppingCart } />
+            <Route exact path="/Product/:id" component={ Product } />
+          </Switch>
+        </div>
+      </ProductProvider>
     </BrowserRouter>
   );
 }
