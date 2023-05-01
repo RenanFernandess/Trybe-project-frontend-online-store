27bec0a30f06d451f260df03d895d4f2522badfc
Renan Fernandes <renzinestuods@gmail.com>
2023-05-01 16:21:53 -0300

    :sparkles: feat: Configura o CartProvider, redenriza o Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Search from './pages/Search';
-import ShoppingCart from './pages/ShoppingCart';
-import Product from './pages/Product';
-import { ProductProvider } from './context';
+import { BrowserRouter } from 'react-router-dom';
+import { ProductProvider, CartProvider } from './context';
 import Header from './components';
+import Routes from './Routes';
 
 function App() {
   return (
     <BrowserRouter>
       <ProductProvider>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/" component={ Search } />
-            <Route exact path="/ShoppingCart" component={ ShoppingCart } />
-            <Route exact path="/Product/:id" component={ Product } />
-          </Switch>
-        </div>
+        <CartProvider>
+          <div className="App">
+            <Header />
+            <Routes />
+          </div>
+        </CartProvider>
       </ProductProvider>
     </BrowserRouter>
   );
