e00064c136a8bea065a9f0f76b3975a7a301a49a
Renan Fernandes <renzinestuods@gmail.com>
2023-05-02 19:37:03 -0300

    :truck: chore: e renomeia o componente CartItem.jsx para ProductCardInCart.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import AddToCartButton from './AddToCartButton';
 import ProductCard from './ProductCard';
 import Header from './Header';
-import ProductCardInCart from './CartItem';
+import ProductCardInCart from './ProductCardInCart';
 
 export default Header;
 export {
