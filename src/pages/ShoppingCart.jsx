6e2f15a99b135031abadc222edca4b8133c489ec
Renan Fernandes <renzinestuods@gmail.com>
2023-05-02 19:43:58 -0300

    :recycle: refactor: Utiliza o context para recuperar as informações do carrinho de compras

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,75 +1,29 @@
 import React, { Component } from 'react';
-import CartItem from '../components/CartItem';
+import { ProductCardInCart } from '../components';
+import { CartContext } from '../context';
 
 export default class ShoppingCart extends Component {
-  state = {
-    shopping: [],
-    elementsId: [],
-  }
-
-  componentDidMount() {
-    const recoveryCart = JSON.parse(localStorage.getItem('cart'));
-    const elementsId = recoveryCart && [...new Set(recoveryCart.map(({ id }) => id))];
-    console.log(elementsId);
-    this.setState({ shopping: recoveryCart || [], elementsId });
-  }
-
-  incrementItem = ({ target }) => {
-    const { shopping } = this.state;
-    const item = shopping.find(({ id }) => target.name === id);
-    shopping.push(item);
-    this.setState({ shopping });
-    localStorage.setItem('cart', JSON.stringify(shopping));
-  };
-
-  reduceItem = ({ target }) => {
-    const { shopping } = this.state;
-    const validation = shopping.filter(({ id }) => id === target.name).length;
-    if (validation !== 1) {
-      const item = shopping.find(({ id }) => target.name === id);
-      const index = shopping.indexOf(item);
-      const products = shopping.filter((_, ind) => ind !== index);
-      this.setState({ shopping: products });
-      localStorage.setItem('cart', JSON.stringify(products));
-    }
-  };
-
-  removeItem = ({ target }) => {
-    const { shopping, elementsId } = this.state;
-    const products = shopping.filter(({ id }) => id !== target.name);
-    const ids = elementsId.filter((id) => id !== target.name);
-    localStorage.setItem('cart', JSON.stringify(products));
-    this.setState({
-      shopping: products,
-      elementsId: ids,
-    });
-  };
-
   render() {
-    const { shopping, elementsId } = this.state;
+    const { cartProducts } = this.context;
     return (
       <div>
-        { (!shopping.length)
-          ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          : (
-            elementsId.map((elemento) => {
-              const products = shopping.filter(({ id }) => elemento === id);
-              const [{ title, id, price, thumbnail }] = products;
-              return (
-                <CartItem
-                  key={ id }
-                  title={ title }
-                  price={ price }
-                  image={ thumbnail }
-                  quantity={ products.length }
-                  incrementItem={ this.incrementItem }
-                  id={ id }
-                  reduceItem={ this.reduceItem }
-                  removeItem={ this.removeItem }
-                />
-              );
-            }))}
+        { cartProducts.length
+          ? (
+            cartProducts.map(({ title, id, price, thumbnail, quantityInCart }, index) => (
+              <ProductCardInCart
+                key={ id }
+                title={ title }
+                price={ price }
+                image={ thumbnail }
+                quantity={ quantityInCart }
+                id={ id }
+                index={ index }
+              />
+            )))
+          : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
       </div>
     );
   }
 }
+
+ShoppingCart.contextType = CartContext;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Search from './Search';
 import Product from './Product';
+import ShoppingCart from './ShoppingCart';
 
 export default Search;
 export {
   Product,
+  ShoppingCart,
 };
