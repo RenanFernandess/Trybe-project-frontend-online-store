5cfaa297ed79c5e2ffd8292ca76ccb04350510dc
Renan Fernandes <renzinestuods@gmail.com>
2023-05-02 15:45:01 -0300

    :bug: fix: Corrige o bug que ao clicar no botão de "Adicionar ao carrinho" mais de uma vez duplicava o elemento em vez de almentar a quantidade

--- src/context/Cart/CartProvider.jsx
+++ src/context/Cart/CartProvider.jsx
@@ -3,41 +3,65 @@ import PropTypes from 'prop-types';
 import CartContext from './CartContext';
 import getItemFromLocalStorage, { saveItemToLocalStorage } from '../../services';
 
-const CART = 'cart';
-const NOT_CONTAIN = -1;
-
 export default class CartProvider extends Component {
   state = {
     cartProducts: [],
   };
 
+  CART_KEY = 'cart'
+
   componentDidMount() {
-    this.setState(getItemFromLocalStorage(CART) || {});
+    this.setState(getItemFromLocalStorage(this.CART_KEY) || {});
+  }
+
+  componentDidUpdate() {
+    this.saveCartToStorage();
   }
 
   addProductInCart = (product) => {
     this.setState(({ cartProducts }) => {
-      const productIndex = cartProducts.indexOf(product);
-      if (productIndex === NOT_CONTAIN) {
-        return { cartProducts: [...cartProducts, product] };
+      let productIndex;
+      const checkIfItContains = cartProducts.some(({ id }, index) => {
+        productIndex = index;
+        return id === product.id;
+      });
+      if (checkIfItContains) {
+        const products = [...cartProducts];
+        products[productIndex] = {
+          ...products[productIndex],
+          quantityInCart: products[productIndex].quantityInCart + 1,
+        };
+        return { cartProducts: products };
       }
-      if (cartProducts[productIndex].quantityInCart) {
-        cartProducts[productIndex].quantityInCart += 1;
-      }
-      cartProducts[productIndex].quantityInCart = 1;
-      return { cartProducts };
-    }, this.saveCartToStorage);
+      product.quantityInCart = 1;
+      return { cartProducts: [...cartProducts, product] };
+    });
   }
 
   changeQuantity = (value, index) => {
     this.setState(({ cartProducts }) => {
-      cartProducts[index].quantityInCart += value;
-      return { cartProducts };
-    }, this.saveCartToStorage);
+      const products = [...cartProducts];
+      products[index].quantityInCart += value;
+      return { cartProducts: products };
+    });
+  }
+
+  // addOnMore = (index) => {
+  //   this.setState(({ cartProducts }) => {
+  //     const products = [...cartProducts];
+  //     products[index].quantityInCart += 1;
+  //     return { cartProducts: products };
+  //   }, this.saveCartToStorage);
+  // }
+
+  removeProduct = (productId) => {
+    const { cartProducts } = this.state;
+    const products = cartProducts.filter(({ id }) => productId !== id);
+    this.setState({ cartProducts: products });
   }
 
   saveCartToStorage = () => {
-    saveItemToLocalStorage(CART, this.state);
+    saveItemToLocalStorage(this.CART_KEY, this.state);
   }
 
   render() {
@@ -47,6 +71,7 @@ export default class CartProvider extends Component {
       ...this.state,
       addProductInCart: this.addProductInCart,
       changeQuantity: this.changeQuantity,
+      removeProduct: this.removeProduct,
     };
 
     return (
