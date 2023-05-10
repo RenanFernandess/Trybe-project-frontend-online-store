39b2135cb57b13f74e5f0cf200f38cc8d1cd6980
Renan Fernandes <renzinestuods@gmail.com>
2023-05-10 18:58:18 -0300

    :sparkles: feat: Cria o componente FreeShipping

---
+++ src/components/FreeShipping.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FreeShipping extends Component {
+  render() {
+    return (
+      <p data-testid="free-shipping">
+        Frete grátis
+      </p>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import AddToCartButton from './AddToCartButton';
 import ProductCard from './ProductCard';
 import Header from './Header';
 import ProductCardInCart from './ProductCardInCart';
+import FreeShipping from './FreeShipping';
 
 export default Header;
 export {
   AddToCartButton,
   ProductCard,
   ProductCardInCart,
+  FreeShipping,
 };
