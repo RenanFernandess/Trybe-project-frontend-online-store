67ba23f6a5b14f94cc65e6735242eb2df6e5f827
Renan Fernandes <renzinestuods@gmail.com>
2023-05-10 19:04:09 -0300

    :sparkles: feat: Renderiza o componente FreeShipping quando tiver frete gratis for verdadeiro

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,14 +3,17 @@ import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Products.css';
 import AddToCartButton from './AddToCartButton';
+import FreeShipping from './FreeShipping';
 
 class ProductCard extends Component {
   render() {
-    const { product: { title, thumbnail, price, id }, product } = this.props;
+    const { product: { title, thumbnail, price, id, shipping: { free_shipping: free,
+    } }, product } = this.props;
     return (
       <div className="product">
         <Link className="link" to={ `/Product/${id}` } data-testid="product-detail-link">
           <div className="product-card" data-testid="product">
+            { free && <FreeShipping /> }
             <img
               className="product-image"
               src={ thumbnail.replace(/(I\.jpg){1}$/g, 'W.jpg') }
@@ -39,6 +42,9 @@ ProductCard.propTypes = {
     thumbnail: propTypes.string.isRequired,
     price: propTypes.number.isRequired,
     id: propTypes.string.isRequired,
+    shipping: propTypes.shape({
+      free_shipping: propTypes.bool.isRequired,
+    }).isRequired,
   }).isRequired,
 };
 
