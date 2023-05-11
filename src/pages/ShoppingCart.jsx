a1e9943061374a963dd984ea4a59675748a6e04f
Renan Fernandes <renzinestuods@gmail.com>
2023-05-11 19:46:26 -0300

    :sparkles: feat: Adiciona o preço total e uma link para finalizar a compra

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { ProductCardInCart } from '../components';
 import { CartContext } from '../context';
 
 export default class ShoppingCart extends Component {
   render() {
-    const { cartProducts } = this.context;
+    const { cartProducts, totalPrice } = this.context;
     return (
       <div>
-        { cartProducts.length
-          ? (
-            cartProducts.map(({ title, id, price, thumbnail, quantityInCart }, index) => (
-              <ProductCardInCart
-                key={ id }
-                title={ title }
-                price={ price }
-                image={ thumbnail }
-                quantity={ quantityInCart }
-                id={ id }
-                index={ index }
-              />
-            )))
-          : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        <section>
+          { cartProducts.length
+            ? (
+              cartProducts
+                .map(({ title, id, price, thumbnail, quantityInCart }, index) => (
+                  <ProductCardInCart
+                    key={ id }
+                    title={ title }
+                    price={ price }
+                    image={ thumbnail }
+                    quantity={ quantityInCart }
+                    id={ id }
+                    index={ index }
+                  />
+                )))
+            : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        </section>
+        <section>
+          <p>
+            Preço total: R$
+            { totalPrice.toFixed(2) }
+          </p>
+          <Link to="/Checkout" data-testid="checkout-products">
+            Finalizar compra
+          </Link>
+        </section>
       </div>
     );
   }
