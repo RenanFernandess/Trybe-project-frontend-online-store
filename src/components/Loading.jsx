0737e8b5c48a72b180ef0feed65067d1107608ca
Renan Fernandes <renzinestuods@gmail.com>
2023-05-11 16:08:28 -0300

    :sparkles: feat: Cria o componente de Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Loading.css';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div className="c-loading">
+        <div className="c-loading__item" />
+      </div>
+    );
+  }
+}
